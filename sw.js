72345458c207e20124920ab5c07daf5e8ac653cd
BackRerd <4012658814@qq.com>
2024-11-15 17:21:25 +0800

    Site updated: 2024-11-15 17:21:24

--- sw.js
+++ sw.js
@@ -6,7 +6,7 @@ const preCache = [
   "/css/style.css",
   "/js/script.js",
 ];
-const VERSION = "1731662366064";
+const VERSION = "1731662475507";
 const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
