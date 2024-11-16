e562c3017ab3dcf0400661075db64920c6212b60
BackRerd <4012658814@qq.com>
2024-11-16 10:59:15 +0800

    Site updated: 2024-11-16 10:59:13

--- sw.js
+++ sw.js
@@ -6,7 +6,7 @@ const preCache = [
   "/css/style.css",
   "/js/script.js",
 ];
-const VERSION = "1731662475507";
+const VERSION = "1731725946795";
 const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
