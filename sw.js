d5939bdacf50fad063b2c22472f32ca6e4d8458a
BackRerd <4012658814@qq.com>
2024-12-18 17:55:14 +0800

    Site updated: 2024-12-18 17:55:12

--- sw.js
+++ sw.js
@@ -6,7 +6,7 @@ const preCache = [
   "/css/style.css",
   "/js/script.js",
 ];
-const VERSION = "1732688356051";
+const VERSION = "1734515708033";
 const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
